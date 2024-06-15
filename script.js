3fee24e8ba59bdecf3332a5c2782d0ac58a2933f
JAHONGIR-AMONOV <jahongir.amonov3010@gmail.com>
2024-06-15 15:54:17 +0500

    even-odd

---
+++ script.js
@@ -0,0 +1,23 @@
+// let arr = ['Alex', 'John', 'Jorge', 'Jasmina', 'faya', 'Amina']
+
+// let callBack = (name) => name.length % 2 === 0
+
+// console.log(
+//     arr.filter(callBack)
+// );
+
+// let num = [1, 2, 3, 4, 5]
+
+// let total = 0
+// num.filter((num) => total = total + num)
+
+// console.log(total);
+
+
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+let even = numbers.filter(number => number % 2 === 0);
+let odd = numbers.filter(number => number % 2 !== 0);
+
+console.log("Even", even);
+console.log("Odd", odd);
